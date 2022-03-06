c98cedc574863fafc5ded70be2fe4fbdddcbcb05
Avellea <dynamicdonkey0@gmail.com>
2022-03-06 17:16:59 -0600

    Add project files.

---
+++ waifu2X-ncnn-gui/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace waifu2X_ncnn_gui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void browseButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+
+            ofd.Title = "Select an image.";
+            ofd.DefaultExt = ".png";
+            ofd.Filter = "Image Files|*.jpg;*.jpeg;*.png";
+
+            if(ofd.ShowDialog() == true)
+            {
+                inputImagePath.Text = ofd.FileName;
+                beforePreview.Source = new BitmapImage(new Uri(ofd.FileName));
+            }
+
+        }
+
+        private void scaleButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            Microsoft.Win32.SaveFileDialog sfd = new Microsoft.Win32.SaveFileDialog();
+            if(sfd.ShowDialog() == true)
+            {
+                string inputPath = inputImagePath.Text;
+                string outputFile = sfd.FileName;
+                string denoiseVal = denoiseValue.Text;
+                string scaleVal = scaleValue.Text;
+
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.CreateNoWindow = false;
+                startInfo.UseShellExecute = false;
+                startInfo.FileName = "waifu2x-ncnn-vulkan.exe";
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                startInfo.Arguments = "-i " + inputPath + " -o " + outputFile + " -n " + denoiseVal + " -s " + scaleVal;
+
+                try
+                {
+                    using (Process exeProcess = Process.Start(startInfo))
+                    {
+                        exeProcess.WaitForExit();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                }
+            }
+
+
+            
+
+        }
+
+    }
+}
