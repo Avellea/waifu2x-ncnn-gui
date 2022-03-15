bb226c4ed9a15a09087823a04e4609f6bbfd29c5
Avellea <dynamicdonkey0@gmail.com>
2022-03-15 14:57:55 -0500

    Add process for building an installer Made waifu2x path configurable.

---
+++ waifu2X-ncnn-gui/ConfigHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Specialized;
+using waifu2X_ncnn_gui;
+
+namespace waifu2x_ncnn_gui
+{
+    class ConfigHandler
+    {
+        public static string GetWaifu2xLocation()
+        {
+            string Waifu2xLocation;
+
+            Waifu2xLocation = ConfigurationManager.AppSettings["waifu2xloc"];
+
+            if(string.IsNullOrEmpty(Waifu2xLocation))
+            {
+                Waifu2xLocation = "Location not set.";
+            }
+
+            return Waifu2xLocation;
+        }
+
+        public static void SetWaifu2xLocation(string loc)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.AppSettings.Settings["waifu2xloc"].Value = loc;
+            config.Save();
+
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+
+    }
+}
--- waifu2X-ncnn-gui/MainWindow.xaml.cs
+++ waifu2X-ncnn-gui/MainWindow.xaml.cs
@@ -2,6 +2,9 @@
 using System.Diagnostics;
 using System.Windows;
 using System.Windows.Media.Imaging;
+using System.Configuration;
+using System.Collections.Specialized;
+using waifu2x_ncnn_gui;
 
 namespace waifu2X_ncnn_gui
 {
@@ -12,13 +15,19 @@ namespace waifu2X_ncnn_gui
     {
         public MainWindow()
         {
+
             InitializeComponent();
+
+            waifu2xLocationInput.Text = ConfigHandler.GetWaifu2xLocation();
+
         }
 
 
 
         private void browseButton_Click(object sender, RoutedEventArgs e)
         {
+
+            
 
             Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
 
@@ -30,6 +39,26 @@ namespace waifu2X_ncnn_gui
             {
                 inputImagePath.Text = ofd.FileName;
                 beforePreview.Source = new BitmapImage(new Uri(ofd.FileName));
+            }
+
+        }
+
+        private void waifu2xLocationBrowse_Click(object sender, RoutedEventArgs e)
+        {
+
+
+
+            Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+
+            ofd.Title = "Select an executable.";
+            ofd.DefaultExt = ".exe";
+            ofd.Filter = "Executable files|*.exe";
+
+            if (ofd.ShowDialog() == true)
+            {
+                waifu2xLocationInput.Text = ofd.FileName;
+                ConfigHandler.SetWaifu2xLocation(waifu2xLocationInput.Text);
+                //beforePreview.Source = new BitmapImage(new Uri(ofd.FileName));
             }
 
         }
@@ -50,7 +79,7 @@ namespace waifu2X_ncnn_gui
                 ProcessStartInfo startInfo = new ProcessStartInfo();
                 startInfo.CreateNoWindow = false;
                 startInfo.UseShellExecute = false;
-                startInfo.FileName = "waifu2x-ncnn-vulkan.exe";
+                startInfo.FileName = waifu2xLocationInput.Text;
                 startInfo.WindowStyle = ProcessWindowStyle.Hidden;
                 startInfo.Arguments = "-i " + '"' + inputPath + '"' + " -o " + '"' + outputFile + '"' + " -n " + denoiseVal + " -s " + scaleVal + " -m " + modelVal + " -g " + gpuVal;
 
